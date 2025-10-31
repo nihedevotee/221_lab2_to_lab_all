28f5493e895e380c07f602c9dc95efd4dd898e56
maheer <younusmohammadmaheer12@gmail.com>
2025-10-31 12:26:31 +0600

    sliding pari na gpt

--- lab2/no5Elab2.java
+++ lab2/no5Elab2.java
@@ -18,14 +18,27 @@ public class no5Elab2 {
         int arr[] = new int[N];
         for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(st2.nextToken());
 
-        int x=0;
-        int y=x+1;
+        int left=0;
+        int y=0;
         int store=0;
-        int same=0;
+        int minus=0;
+        int i=0;
+        int count=0;
+        int max=Integer.MIN_VALUE;
 
-        while (x<N && y< N) {
+        for (int right = 0; right < arr.length; right++) {
+            store += arr[right];
             
+            if(store>K && left<=right){
+                store -= arr[left++];
+            }
+            
+            count=right-left+1;
+
+            max=Math.max(count, max);
         }
+        pw.println(max);
+        
 
     }
 }

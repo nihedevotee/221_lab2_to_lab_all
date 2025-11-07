f20bce258780e30c738e83d3b2e0d94af24db2f4
maheer <younusmohammadmaheer12@gmail.com>
2025-11-07 15:02:17 +0600

    .

--- lab3/no5Elab3.java
+++ lab3/no5Elab3.java
@@ -1,65 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class no5Elab3 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        PrintWriter pw = new PrintWriter(System.out, true);
+        PrintWriter pw = new PrintWriter(System.out);
         int t = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < t; i++) {
-
             StringTokenizer st = new StringTokenizer(br.readLine());
-
             long a = Long.parseLong(st.nextToken());
             long n = Long.parseLong(st.nextToken());
             long m = Long.parseLong(st.nextToken());
 
-            if (a == 1) {
-                pw.println(n % m);
-            } else {
-                long num=(expo(a, n + 1, m) - a + m) % m;
-                long deno= (a-1 + m)% m;
-                long inv= expo (deno,m-2,m);
-                //long inv = modInverse(deno, m);
-                long sum = (num * inv)%m;
+            long res;
 
-                // sum=sum%m;
-                // if (sum<0) {
-                //     sum = sum + m;
-                // }
-                pw.println(sum);
+            if (a == 1) {
+                res = n % m;
+            } else {
+                long num = (expo(a, n + 1, m) - a + m) % m;
+                long deno = (a - 1 + m) % m;
+                long inv = expo(deno, m - 2, m);
+                res = (num * inv) % m;
             }
+
+            pw.println(res);
         }
+
+        pw.flush();
+        pw.close();
+        br.close();
     }
 
-    // public static long expo(long a, long b, long m) {
-    //     if (b == 0) {
-    //         return 1;
-    //     }
-    //     a=a%m;
-
-    //     long mid = b / 2;
-    //     long y = expo(a, mid, m);
-    //     long x = y * y;
-
-    //     if (b % 2 == 0) {
-    //         return x % m;
-    //     } else {
-    //         return (x * a) % m;
-    //     }
-    // }
-    public static long expo(long a, long b, long m) {
+    private static long expo(long a, long b, long m) {
         long res = 1;
         a %= m;
+
         while (b > 0) {
-            if ((b % 2) == 1) res = (res * a) % m;
+            if ((b & 1) == 1)
+                res = (res * a) % m;
             a = (a * a) % m;
-            b = b/2;
+            b >>= 1;
         }
+
         return res;
     }
-
 }

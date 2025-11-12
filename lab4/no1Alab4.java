ed4dcaccd34e026b16812a7eb389a143da51fc6b
maheer <younusmohammadmaheer12@gmail.com>
2025-11-12 15:55:25 +0600

    .

--- lab4/no1Alab4.java
+++ lab4/no1Alab4.java
@@ -1,5 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+//import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
 public class no1Alab4{
-    public static void main (String [] args){
-        System.out.println("hi");
+    public static void main (String [] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out, true);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int [][] mat = new int [n][n];
+        //int [] c = new int [n];
+        for (int i = 0; i < m; i++) {
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            mat[Integer.parseInt(st1.nextToken())-1][Integer.parseInt(st1.nextToken())-1]=Integer.parseInt(st1.nextToken());
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                
+                pw.print(mat[i][j]+" ");
+            }
+            pw.println();
+        }
+        pw.flush();
     }
 }

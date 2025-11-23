bafbeb4f875d52f3b391711b5e2334278d764587
maheer <younusmohammadmaheer12@gmail.com>
2025-11-24 00:33:12 +0600

    ..

---
+++ lab4/no7Glab4.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+//import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class no7Glab4 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter pw = new PrintWriter(System.out, true);
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        
+        for (int i = 0; i < k; i++) {
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+        }
+
+    }
+}
